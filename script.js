917ac76c9efe344746dc59e9b3b50302a223da5d
Szilágyi Péter <szilagyi.peter96@gmail.com>
2020-03-29 20:59:16 +0200

    03.29 20:59

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
-let navbar = document.getElementById("navbar");
-let hamburger = document.getElementById("hamburger");
-let checkbox = document.getElementById("nav_button");
+// let navbar = document.getElementById("navbar");
+// let hamburger = document.getElementById("hamburger");
+// let checkbox = document.getElementById("nav_button");
 
-function OpenNavBar() {
-    navbar.style.maxWidth = "none";
+let index = document.getElementById("index");
+let menu = document.getElementById("menu");
+let booking = document.getElementById("booking");
+let info = document.getElementById("info");
+
+let index_Link = document.getElementById("index_Link");
+let menu_Link = document.getElementById("menu_Link");
+let booking_Link = document.getElementById("booking_Link");
+let info_Link = document.getElementById("info_Link");
+
+
+// function OpenNavBar() {
+//     navbar.style.maxWidth = "none";
+// }
+
+// function CloseNavBar() {
+//     navbar.style.maxWidth = "48px";
+// }
+
+// hamburger.onclick = function() {
+
+//     if(checkbox.checked){
+//         CloseNavBar();
+//     }else {
+//         OpenNavBar();
+//     }
+
+// };
+
+function hideAll() {
+    index.style.display = "none";
+    menu.style.display = "none";
+    booking.style.display = "none";
+    info.style.display = "none";
+
+    index_Link.className = "";
+    menu_Link.className = "";
+    booking_Link.className = "";
+    info_Link.className = "";
 }
 
-function CloseNavBar() {
-    navbar.style.maxWidth = "48px";
+function changeToIndex() {
+    console.log("index!");
+
+    hideAll();
+    index.style.display = "block";
+    index_Link.className = "selected";
 }
 
-hamburger.onclick = function() {
+function changeToMenu() {
+    console.log("menu!");
 
-    if(checkbox.checked){
-        CloseNavBar();
-    }else {
-        OpenNavBar();
-    }
+    hideAll();
+    menu.style.display = "block";
+    menu_Link.className = "selected";
+}
 
-};
+function changeToBooking() {
+    console.log("booking!");
+
+    hideAll();
+    booking.style.display = "block";
+    booking_Link.className = "selected";
+}
+
+function changeToInfo() {
+    console.log("info!");
+
+    hideAll();
+    info.style.display = "block";
+    info_Link.className = "selected";
+}
