9b5d9bf8b6da2e74f1954cb9d1c1f079e94bf287
Szilágyi Péter <szilagyi.peter96@gmail.com>
2020-04-04 09:53:22 +0200

    04.02?

--- menu_data.js
+++ menu_data.js
@@ -1,7 +1,24 @@
 let menuArray = new Array();
+let icons = {
+    meat: "images/icons/009-meat.png",
+    egg: "images/icons/001-fried-egg.png",
+    wheat: "images/icons/025-wheat.png",
+    bread: "images/icons/012-bread.png",
+    radish: "images/icons/016-radish.png",
+    avocado: "images/icons/024-avocado.png",
+    mushroom: "images/icons/037-mushroom-1.png",
+    pepper: "images/icons/041-pepper.png",
+    corn: "images/icons/063-corn.png",
+    fish: "images/icons/077-fish-1.png",
+    milk: "images/icons/084-milk.png",
+    onion: "images/icons/087-onion.png",
+    lemon: "images/icons/095-lemon.png",
+    cheese: "images/icons/098-cheese.png"
+}
 
 let food_1 = {
     name: "Avocado Toast" ,
+    icons: ["bread", "radish", "avocado"],
     description: "Whole wheat toasted bread with avocado, radish, salad and tomatoes.",
     img: "images/foods/avocado_toast.jfif"
 };
@@ -9,6 +26,7 @@ menuArray.push(food_1);
 
 let food_2 = {
     name: "Chicken Sandwich",
+    icons: ["meat", "bread", "onion"],
     description: "Bread sandwich with onions, chicken breast and salad.",
     img: "images/foods/chicken_sandwich.jfif"
 };
@@ -16,6 +34,7 @@ menuArray.push(food_2);
 
 let food_3 = {
     name: "Nacho Burger",
+    icons: ["meat", "bread", "onion", "corn"],
     description: "Beef patty, red onion, mayo based sauce and tortilla chips in a whole wheat bun.",
     img: "images/foods/chips_burger.jfif"
 };
@@ -23,6 +42,7 @@ menuArray.push(food_3);
 
 let food_4 = {
     name: "Cucumber Wrap",
+    icons: ["wheat", "onion"],
     description: "Tortilla wrap with cucumber, chives and mayo sauce.",
     img: "images/foods/cucumber_wrap.jfif"
 };
@@ -30,7 +50,25 @@ menuArray.push(food_4);
 
 let food_5 = {
     name: "BBQ Wings",
+    icons: ["meat", "corn", "onion"],
     description: "Chicken wings grilled BBQ style, served with corn cobs.",
     img: "images/foods/grilled_corn.jfif"
 };
 menuArray.push(food_5);
+
+let food_6 = {
+    name: "Coconut Salmon",
+    icons: ["fish", "lemon", "bread"],
+    description: "Salmon fillet with salad, lemon, and coconut slices.",
+    img: "images/foods/salmon_coconut_plate.jfif"
+};
+menuArray.push(food_6);
+
+
+let food_7 = {
+    name: "Vegan tacos",
+    icons: ["wheat", "corn", "avocado", "onion", "pepper"],
+    description: "Taco shells filled with avocado, corn, red onions and beans, served with hot salsa and garlic dips.",
+    img: "images/foods/vegan_tortillas.jfif"
+};
+menuArray.push(food_7);
--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 // let hamburger = document.getElementById("hamburger");
 // let checkbox = document.getElementById("nav_button");
 
+// DOM elements for the four "pages"
 let index = document.getElementById("index");
 let menu = document.getElementById("menu");
 let booking = document.getElementById("booking");
 let info = document.getElementById("info");
 
+// DOM elements for nav links
 let index_Link = document.getElementById("index_Link");
 let menu_Link = document.getElementById("menu_Link");
 let booking_Link = document.getElementById("booking_Link");
@@ -32,6 +34,7 @@ let info_Link = document.getElementById("info_Link");
 
 // };
 
+//hides all pages
 function hideAll() {
     index.style.display = "none";
     menu.style.display = "none";
@@ -43,6 +46,8 @@ function hideAll() {
     booking_Link.className = "";
     info_Link.className = "";
 }
+
+// functions to reveal each page
 
 function changeToIndex() {
     console.log("index!");
@@ -75,35 +80,50 @@ function changeToInfo() {
     info.style.display = "block";
     info_Link.className = "selected";
 }
+//
 
+// Generate the menu
 for (let i = 0; i < menuArray.length; i++) {
 
-    console.log(menuArray[i]);
-    
+    // Create new divs for the menu entry, the text div inside, and the icon div inside that.
     let menu_div = document.createElement('div');
-
     let text_div = document.createElement('div');
+    let icon_div = document.createElement('div');
 
+    // Create Menu image
     let image = document.createElement('img');
     image.src = menuArray[i].img;
 
+    // Create heading for menu item name
     let name = document.createElement('h2');
     name.innerHTML = menuArray[i].name;
 
+    // Create a paragraph for description
     let description = document.createElement('p');
     description.innerHTML = menuArray[i].description;
 
+    // This places the image left or right alternating.
     if(i % 2 === 0){
         menu_div.appendChild(image);
         text_div.appendChild(name);
+        text_div.appendChild(icon_div);
         text_div.appendChild(description);
         menu_div.appendChild(text_div);
     } else {
         text_div.appendChild(name);
+        text_div.appendChild(icon_div);
         text_div.appendChild(description);
         menu_div.appendChild(text_div);
         menu_div.appendChild(image);
     }
+    
+    //Populate the icons div, icons are determined in menu_data.js
+    for (let j = 0; j < menuArray[i].icons.length; j++) {
+        let new_icon = document.createElement('img'); 
+        new_icon.src = icons[menuArray[i].icons[j]];
+        icon_div.appendChild(new_icon);
+    }
 
+    //Finally add the whole thing to the menu page
     menu.appendChild(menu_div);
 }
